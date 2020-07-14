c71a5b7445bdcc5dc926204b8cd26c5c0425cc2c
Alessandro Menezes <alessandroadm@live.com>
2020-07-14 17:22:18 -0300

    Feat(portal) Adding challenges migration

---
+++ migrations/20200714201701-create_challenges.js
@@ -0,0 +1,88 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('challenges', {
+      'id': {
+        type: Sequelize.INTEGER(11),
+        allowNull: false,
+        primaryKey: true,
+        comment: "null",
+        autoIncrement: true
+      },
+      'title': {
+        type: Sequelize.STRING(80),
+        allowNull: false,
+        comment: "null"
+      },
+      'subtitle': {
+        type: Sequelize.STRING(80),
+        allowNull: false,
+        comment: "null"
+      },
+      'slides': {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        defaultValue: 'default',
+        comment: "null"
+      },
+      'text': {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        comment: "null"
+      },
+      'score': {
+        type: Sequelize.INTEGER(11),
+        allowNull: false,
+        defaultValue: '0',
+        comment: "null"
+      },
+      'expiresAt': {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        comment: "null"
+      },
+      'createdAt': {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        comment: "null"
+      },
+      'updatedAt': {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        comment: "null"
+      },
+      'xp': {
+        type: Sequelize.INTEGER(11),
+        allowNull: false,
+        defaultValue: '0',
+        comment: "null"
+      },
+      'moduleId': {
+        type: Sequelize.INTEGER(11),
+        allowNull: false,
+        comment: "null",
+        references: {
+          model: 'Module',
+          key: 'id'
+        }
+      },
+      'statusId': {
+        type: Sequelize.INTEGER(11),
+        allowNull: false,
+        comment: "null",
+        references: {
+          model: 'Challenge_status',
+          key: 'id'
+        }
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('challenges');
+  }
+};
