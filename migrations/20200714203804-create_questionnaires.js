67cb05008c0618f80ef5d7e6bfbdeee757b6e91a
Alessandro Menezes <alessandroadm@live.com>
2020-07-14 17:40:56 -0300

    Feat(portal) Adding questionnaires migration

---
+++ migrations/20200714203804-create_questionnaires.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('questionnaires', {
+      'id': {
+        type: Sequelize.INTEGER(11),
+        allowNull: false,
+        primaryKey: true,
+        comment: "null",
+        autoIncrement: true
+      },
+      'classId': {
+        type: Sequelize.INTEGER(11),
+        allowNull: false,
+        comment: "null",
+        references: {
+          model: 'classes',
+          key: 'id'
+        }
+      },
+      'title': {
+        type: Sequelize.STRING(45),
+        allowNull: false,
+        comment: "null"
+      },
+      'ask': {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        comment: "null"
+      },
+      'rightAnswerId': {
+        type: Sequelize.INTEGER(11),
+        allowNull: false,
+        comment: "null"
+      },
+      'createdAt': {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        comment: "null"
+      },
+      'updatedAt': {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        comment: "null"
+      },
+      'avaliable': {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: '0',
+        comment: "null"
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+     await queryInterface.dropTable('questionnaires');
+  }
+};
