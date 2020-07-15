33a0917fff4129e9d9f101ff9f130f40532a0a0e
Alessandro Menezes <alessandroadm@live.com>
2020-07-15 00:37:03 -0300

    Feat(Portal) Adding testes to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,26 @@
 const { User } = require('../models');
+const validator = require('validator');
+ 
 module.exports = {
     index: async () => {
         const users = await User.findAll();
         return users;
-    }  
+    },
+    
+    store: async (user) => {
+        if (!user.email || !user.password) {
+            return 'Informe, email e senha';
+        }
+        console.log(!(validator.isEmail(user.email)), user.email);
+        if (!(validator.isEmail(user.email))) {
+            return 'Email inválido'; 
+        }
+        const alreadyExistis = await User.findOne({ where: { email: user.email } });
+        if (alreadyExistis) {
+            return 'Email já cadastrado'; 
+        }
+        user.email = user.email.toLowerCase();
+        const result = await User.create(user);
+        return result;
+    }
 };
--- tests/userController.test.js
+++ tests/userController.test.js
@@ -1,11 +1,58 @@
 const userController = require('../controllers/userController');
+const {User} = require('../models');
 
-test('Listar usuários - Deve retornar 6', async () => {
+//beforeAll(async() => await User.sequelize.close());
+afterAll(async() => await User.sequelize.close());
+
+test('Listar usuários', async () => {
     let users = await userController.index();
-    expect(users.length).toBe(6);
+    expect(users[5].email).toBe('alessandroadm@live.com');
+});
+
+test('Deve retornar mais de 5 usuários', async () => {
+    let users = await userController.index();
+    expect(users.length).toBeGreaterThan(5);
 });
 
 test('O primeiro usuário deve ter id: 1 ', async () => {
     let users = await userController.index();
     expect(users[0].id).toBe(1);
+});
+
+test('Não pode aceitar valores vazios', async () => {
+    let user = {};
+    let result = await userController.store(user);
+    expect(result).toBe('Informe, email e senha');
+});
+
+test('Não pode aceitar salvar sem informar uma senha', async () => {
+    let user = {name:'Username'};
+    let result = await userController.store(user);
+    expect(result).toBe('Informe, email e senha');
+});
+
+test('Deve retornar email inválido', async () => {
+    let user = {email:'username', password:'123'};
+    let result = await userController.store(user);
+    expect(result).toBe('Email inválido');
+});
+
+test('Deve retornar email já cadastrado', async () => {
+    let user = {email:'alessandroadm@live.com', password:'123'};
+    let result = await userController.store(user);
+    expect(result).toBe('Email já cadastrado');
+});
+
+test('Deve aceitar salvar', async () => {
+    let n = parseInt(Math.random() * 1000);
+    let user = {email:`username${n}@mail.com`, password:'123'};
+    let { email, password } = await userController.store(user);
+    expect({email, password}).toEqual({email:`username${n}@mail.com`, password:'123'});
+});
+
+test('Deve normalizar o email em lowercase', async () => {
+    let n = parseInt(Math.random() * 1000);
+    let user = {email:`USERNAME${n}@maiL.cOm`, password:'123'};
+    let { email, password } = await userController.store(user);
+    expect({email, password}).toEqual({email:`username${n}@mail.com`, password:'123'});
 });
