0bb9a9543c1fe65a345f529b226f8630283776ff
Alessandro Menezes <alessandroadm@live.com>
2020-07-26 14:56:40 -0300

    feat(questionnairequestion): adding method store
    link a question in the questionnaire

---
+++ controllers/questionnaireQuestionController.js
@@ -0,0 +1,23 @@
+const { QuestionnaireQuestion, Question, Questionnaire } = require('../models');
+
+module.exports = {
+
+    store: async (questionnaireQuestion) => {
+        try {
+            const questionnaireExist = await Questionnaire.findByPk(questionnaireQuestion.questionnaireId);
+            if (!questionnaireExist) {
+                return { error: true, status: 422, msg: 'O questionário não está mais disponível' };
+            }
+            const questionExist = await Questionnaire.findByPk(questionnaireQuestion.questionId);
+            if (!questionExist) {
+                return { error: true, status: 422, msg: 'O questão não está mais disponível' };
+            }
+            const result = await QuestionnaireQuestion.create(questionnaireQuestion);
+            return result;
+        } catch (error) {
+            console.log(error);
+            return { error: true, status: 422, msg: error.message };
+        }
+    }
+
+};
