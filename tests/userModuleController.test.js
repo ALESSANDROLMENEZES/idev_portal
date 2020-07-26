162c397f1290dd22264c2bc7a1d549471a48636d
Alessandro Menezes <alessandroadm@live.com>
2020-07-25 21:30:33 -0300

    test(usermodule): adding method delete

--- tests/userModuleController.test.js
+++ tests/userModuleController.test.js
@@ -9,6 +9,11 @@ test('Deve dar acesso para o usuário id 1 para o módulo id 2 ', async () => {
 
 test('Deve listar todos os módulos disponíveis para o usuário id 1 ', async () => {
     const list = await userModuleController.index();
-    console.log(list)
     expect(list.size).toBeGreaterThan(1);
+});
+
+test('Deve excluir o módulo que foi salvo ', async () => {
+    await userModuleController.destroy(userModule);
+    const list = await userModuleController.index();
+    expect(list.size).toBe(1);
 });
