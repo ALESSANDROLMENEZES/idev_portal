38e2e8e83d7c3d821197a1de5873af2b71ffd184
Alessandro Menezes <alessandroadm@live.com>
2020-07-26 14:30:44 -0300

    test(questionnaire): fix update method on test
    new questionnaire receive id after save

--- tests/questionnaireController.test.js
+++ tests/questionnaireController.test.js
@@ -8,11 +8,13 @@ const newQuestionnaire = {
 
 test('Deve salvar um novo questionário ', async () => {
     const result = await questionnaireController.store(newQuestionnaire);
+    newQuestionnaire.id = result.id;
     expect(result.title).toBe('Salvando teste');
 });
 
 test('Deve atualizar o questionário que foi salvo anteriormente ', async () => {
-    newQuestionnaire.title('Titulo atualizado');
+    newQuestionnaire.title = 'Titulo atualizado';
     const result = await questionnaireController.update(newQuestionnaire);
+    console.log(result);
     expect(result.title).toBe('Titulo atualizado');
 });
