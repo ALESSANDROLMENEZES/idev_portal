1418339dc522c1df6c156c58cad70237137ec6a8
Alessandro Menezes <alessandroadm@live.com>
2020-07-26 15:26:07 -0300

    feat(answer): adding destroy method

--- controllers/answerController.js
+++ controllers/answerController.js
@@ -10,6 +10,38 @@ module.exports = {
             console.log(error);
             return { error: true, status:422, msg:error.message};
         }
-    }
+    },
+
+    update: async (answer) => {
+        try {
+            const answerExist = await Answer.findByPk(answer.id);
+            if (!answerExist) {
+                return { error: true, status:422, msg:'A resposta informada não foi encontrada'};    
+            }
+            answerExist.description = answer.description;
+            answerExist.avaliable = answer.avaliable;
+            const result = await answerExist.save();
+            return result;
+        } catch (error) {
+            console.log(error);
+            return { error: true, status:422, msg:error.message};
+        }
+    },
+
+    destroy: async (id) => {
+        try {
+            const answerExist = await Answer.findByPk(id);
+            if (!answerExist) {
+                return { error: true, status:422, msg:'A resposta informada não foi encontrada'};    
+            }
+            const result = await answerExist.destroy();
+            return result;
+        } catch (error) {
+            console.log(error);
+            return { error: true, status:422, msg:error.message};
+        }
+    },
+
+    
     
 };
