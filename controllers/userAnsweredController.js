35854303dbf762560edb9a6cb16e880cb639e28e
Alessandro Menezes <alessandroadm@live.com>
2020-07-26 16:00:54 -0300

    feat(useranswered): adding update method

--- controllers/userAnsweredController.js
+++ controllers/userAnsweredController.js
@@ -1,7 +1,7 @@
 const { UserAnswered, Question, Answer } = require('../models');
 
 module.exports = {
-
+    
     store: async (userAnswered) => {
         const transaction = await UserAnswered.sequelize.transaction();
         try {
@@ -12,19 +12,34 @@ module.exports = {
             const answerExist = await Answer.findByPk(userAnswered.answerId);
             
             if (!answerExist) {
-                return { error: true, status: 422, msg: 'Não foi a resposta informada' };
+                return { error: true, status: 422, msg: 'Não foi encontrado a resposta informada' };
             }
-
+            
             const result = await UserAnswered.create(userAnswered);
             await transaction.commit();
             return result;
-
+            
         } catch (error) {
             await transaction.rollback();
             console.log(error);
             return { error: true, status: 422, msg: error.message };
         }
     },
-
-
+    
+    update: async (userAnswered) => {
+        const transaction = await UserAnswered.sequelize.transaction();
+        try {
+            const userAnswerExist = await UserAnswered.findByPk(userAnswered.id);
+            if (!userAnswerExist) {
+                return { error: true, status: 422, msg: 'Não foi encontrado a resposta informada' };
+            }
+            const result = await UserAnswered.update(userAnswered, { where: { id: userAnswered.id } });
+            return result;
+        } catch (error) {
+            await transaction.rollback();
+            console.log(error);
+            return { error: true, status: 422, msg: error.message };
+        }
+    }
+    
 };
