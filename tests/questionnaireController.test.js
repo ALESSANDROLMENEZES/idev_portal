e20877d6f6665644e1903c1b260e198831ba83c7
Alessandro Menezes <alessandroadm@live.com>
2020-07-26 14:21:53 -0300

    test(questionnaire): addint update test

--- tests/questionnaireController.test.js
+++ tests/questionnaireController.test.js
@@ -10,3 +10,9 @@ test('Deve salvar um novo questionário ', async () => {
     const result = await questionnaireController.store(newQuestionnaire);
     expect(result.title).toBe('Salvando teste');
 });
+
+test('Deve atualizar o questionário que foi salvo anteriormente ', async () => {
+    newQuestionnaire.title('Titulo atualizado');
+    const result = await questionnaireController.update(newQuestionnaire);
+    expect(result.title).toBe('Titulo atualizado');
+});
