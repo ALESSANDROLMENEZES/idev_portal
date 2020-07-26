6d8dbb3c84cf5e4810e12a093208d793d7f08dc9
Alessandro Menezes <alessandroadm@live.com>
2020-07-26 14:09:52 -0300

    feat(questionnaire): adding method update
    if some questionnaire need to be alter

--- controllers/questionnaireController.js
+++ controllers/questionnaireController.js
@@ -54,10 +54,30 @@ module.exports = {
                 return { error: true, status: 422, msg: 'O questionário informado não está disponível' };
             }
             const result = questionnaireExist.destroy();
-            transaction.commit();
+            await transaction.commit();
             return result;
         } catch (error) {
-            transaction.rollback();
+            await transaction.rollback();
+            console.log(error);
+            return { error: true, status: 422, msg: error.message }; 
+        }
+    },
+
+    update: async (questionnaire) => {
+        const transaction = await Questionnaire.sequelize.transaction();      
+        try {
+            const questionnaireExist = await Questionnaire.findByPk(questionnaire.id);
+            if (!questionnaireExist) {
+                return { error: true, status: 422, msg: 'O questionário informado não está disponível' };
+            }
+            questionnaireExist.classId = questionnaire.classId;
+            questionnaireExist.title = questionnaire.title;
+            questionnaireExist.avaliable = questionnaire.avaliable;
+            const result = await questionnaireExist.save();
+            await transaction.commit();
+            return result;
+        } catch (error) {
+            await transaction.rollback();
             console.log(error);
             return { error: true, status: 422, msg: error.message }; 
         }
