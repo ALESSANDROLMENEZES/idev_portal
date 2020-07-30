1ccfc7bff49ac0eccbebb23ae11566730b039c13
Alessandro <alessandroadm@live.com>
2020-07-29 22:28:43 -0300

    feat(routes): adding team routes

--- routes/teams.js
+++ routes/teams.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const teamController = require('../controllers/teamController');
 
-// router.get('/', controller...);
-// router.get('/', controller...);
-// router.post('/', controller...);
-// router.patch('/', controller...);
-// router.delete('/', controller...);
-// router.put('/', controller...);
+router.get('/all', teamController.index);
+router.get('/:id', teamController.show);
+router.post('/', teamController.store);
+router.patch('/', teamController.update);
+router.delete('/:id', teamController.destroy);
+router.put('/', teamController.update);
 
 module.exports = router;
