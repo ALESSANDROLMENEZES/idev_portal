34b95234606d5f5eed39506e596b92a1db0204de
Alessandro <alessandroadm@live.com>
2020-07-29 22:38:10 -0300

    feat(routes): adding user routes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
 
-// router.get('/', controller...);
+router.delete('/:id', userController.destroy);
+router.put('/', userController.update);
+router.patch('/', userController.update);
+router.get('/all', userController.index);
+router.get('/:id?', userController.show);
 
 module.exports = router;
