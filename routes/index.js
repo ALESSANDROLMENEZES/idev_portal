0eb23eb645cdf86e07b31e23861313e99fc783ec
Alessandro <alessandroadm@live.com>
2020-07-29 22:16:26 -0300

    feat(routes): adding index routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
 
-// router.get('/', controller...);
-// router.get('/', controller...);
-// router.post('/', controller...);
-// router.patch('/', controller...);
-// router.delete('/', controller...);
-// router.put('/', controller...);
+router.get('/', userController.index);
+router.get('/', userController.show);
+router.post('/', userController.store);
+router.post('/login', userController.login);
 
 module.exports = router;
