3fc7864c1a80d93c51256096a281032c09995f62
Alessandro <alessandroadm@live.com>
2020-07-29 22:04:35 -0300

    feat(routes): adding challenge routes

--- routes/challenges.js
+++ routes/challenges.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
-// router.get('/', controller...);
-// router.get('/', controller...);
-// router.post('/', controller...);
-// router.patch('/', controller...);
-// router.delete('/', controller...);
-// router.put('/', controller...);
+const challengeController = require('../controllers/challengeController');
+router.get('/', challengeController.index);
+router.get('/:id', challengeController.show);
+router.post('/', challengeController.store);
+router.patch('/', challengeController.update);
+router.delete('/:id', challengeController.destroy);
+router.put('/', challengeController.update);
 
 module.exports = router;
