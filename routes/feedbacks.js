2dbea2f9adc24e3ad88950c191f13daf8732fea6
Alessandro <alessandroadm@live.com>
2020-07-29 22:57:32 -0300

    feat(routes): adding feedbackstatus on feedback routes

--- routes/feedbacks.js
+++ routes/feedbacks.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const feedbackController = require('../controllers/feedbackController');
+const feedbackStatusController = require('../controllers/feedbackStatusController');
+
 router.get('/all', feedbackController.index);
 router.get('/:id', feedbackController.show);
 router.post('/', feedbackController.store);
 router.patch('/', feedbackController.store);
 router.delete('/:id', feedbackController.destroy);
 router.put('/', feedbackController.store);
+router.post('/status', feedbackStatusController.store);
+router.get('/status/all', feedbackStatusController.index);
+router.get('/status/:id', feedbackStatusController.show);
+router.delete('/status/:id', feedbackStatusController.destroy);
+router.put('/status/:id', feedbackStatusController.update);
+router.patch('/status/:id', feedbackStatusController.update);
 
 module.exports = router;
