42ba0f63d5f9f7926eb54e23da6307c4b08cec76
Alessandro <alessandroadm@live.com>
2020-07-29 22:50:45 -0300

    feat(routes): adding usermodule on modules routes

--- routes/modules.js
+++ routes/modules.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const moduleController = require('../controllers/moduleClassesController');
+const userModuleController = require('../controllers/userModuleController');
 
 router.post('/', moduleController.store);
 router.delete('/', moduleController.destroy);
+router.post('/usermodule', userModuleController.store);
+router.get('/usermodule', userModuleController.index);
+router.destroy('/usermodule/:userId/:moduleId', userModuleController.destroy);
 
 module.exports = router;
