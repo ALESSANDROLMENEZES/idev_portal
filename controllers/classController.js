3f1515498bac390189b0291a4bbde51a5d572c1a
Alessandro <alessandroadm@live.com>
2020-08-04 23:33:17 -0300

    feat(routes): adding validations in classes routes

--- controllers/classController.js
+++ controllers/classController.js
@@ -1,10 +1,16 @@
-const { Class, Module } = require('../models');
-
+const { Class, Module, ModuleClass } = require('../models');
+const { validationResult } = require('express-validator');
 
 module.exports = {
     
     index: async (req, res) => {
         try {
+            
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            
             const { moduleId } = req.params;
             const { rows: result, count: size } = await Class.findAndCountAll({
                 include: [
@@ -26,6 +32,12 @@ module.exports = {
     
     show: async (req, res) => {
         try {
+            
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            
             const { id } = req.params;
             const result = await Class.findByPk(id, {
                 include: [
@@ -49,86 +61,112 @@ module.exports = {
     
     store: async (req, res) => {
         try {
+            
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            
             let msg;
+            let { moduleId } = req.params;
             const { title, subtitle, resume, text, code, slides, video, score, xp } = req.body;
-            let _class = { title, subtitle, resume, text, code, slides, video, score, xp };
             
             if (!slides.includes('/embed?') || !slides.includes('https://docs')) {
-                msg = 'Informe um link válido do google slides com a propriedade: /embed?';
-                return res.status(422).json({ error: true, msg});
-            }
-        
-            if (!video.includes('/embed') || !video.includes('https://www.youtube')) {
-                msg = 'Informe um link válido do youtube com a propriedade: /embed?';
-                return res.status(422).json({ error: true, msg});
-            }
-    
-            const result = await Class.create(_class);
-            return res.status(200).json({ result });
-    
-        } catch (error) {
-            console.log(error);
-            return res.status(422).json({ error: true, msg:error.message});
+            msg = 'Informe um link válido do google slides com a propriedade: /embed?';
+            return res.status(422).json({ error: true, msg});
         }
-    },
-
-
-    update: async (req, res) => {
-        const transaction = await Class.sequelize.transaction();
-        try {
-            const { id } = req.params;
-            const { title, subtitle, resume, text, code, slides, video, score, xp } = req.body;
-            let _class = { title, subtitle, resume, text, code, slides, video, score, xp };
         
-            const foundClass = await Class.findByPk(id);
-            if (!foundClass) {
-                return res.status(422).json({ error: true, msg:'Não foi encontrada uma aula com o id especificado'});
-            }
-        
-            if (!slides.includes('/embed?') || !slides.includes('https://docs')) {
-                msg = 'Informe um link válido do google slides com a propriedade: /embed?';
-                return res.status(422).json({ error: true, msg});
-            }
-    
-            if (!video.includes('/embed') || !video.includes('https://www.youtube')) {
-                msg = 'Informe um link válido do youtube com a propriedade: /embed?';
-                return res.status(422).json({ error: true, msg});
-            }
-
-            const result = await Class.update(_class, { where: { id } });
-
-            await transaction.commit();
-
-            return res.status(200).json({ result });
-
-        } catch (error) {
-            await transaction.rollback();
-            console.log(error);
-            return res.status(422).json({ error: true, msg:error.message});
+        if (!video.includes('/embed') || !video.includes('youtube')) {
+            msg = 'Informe um link válido do youtube com a propriedade: /embed?';
+            return res.status(422).json({ error: true, msg});
         }
-    },
-
-
-    destroy: async (req, res) => {
-        const transaction = await Class.sequelize.transaction();
-        try {
-            let { id } = req.params;
-            const foundClass = await Class.findByPk(id);
-            if (!foundClass) {
-                return res.status(422).json({ error: true, msg:'Não foi encontrada uma aula com o id especificado'});
-            }
-            const result = await foundClass.destroy();
         
-            await transaction.commit();
+        const moduleExist = await Module.findByPk(moduleId);
         
-            return res.status(200).json({ result });
-        
-        } catch (error) {
-            await transaction.rollback();
-            console.log(error);
-            return res.status(422).json({ error: true, msg: error.message});
+        if (!moduleExist) {
+            return res.status(422).json({ error: true, msg:'Não foi encontrado um módulo com o id informado'});
         }
+        
+        const savedClass = await Class.create({ title, subtitle, resume, text, code, slides, video, score, xp });
+        
+        const result = await ModuleClass.create({ moduleId, classId: savedClass.id });
+        
+        return res.status(200).json({ result, savedClass });
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(422).json({ error: true, msg:error.message});
     }
+},
+
+
+update: async (req, res) => {
+    const transaction = await Class.sequelize.transaction();
+    try {
+        
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        
+        const { id } = req.params;
+        const { title, subtitle, resume, text, code, slides, video, score, xp } = req.body;
+        
+        const foundClass = await Class.findByPk(id);
+        if (!foundClass) {
+            return res.status(422).json({ error: true, msg:'Não foi encontrada uma aula com o id especificado'});
+        }
+        
+        if (!slides.includes('/embed?') || !slides.includes('https://docs')) {
+        msg = 'Informe um link válido do google slides com a propriedade: /embed?';
+        return res.status(422).json({ error: true, msg});
+    }
+    
+    if (!video.includes('/embed') || !video.includes('youtube')) {
+        msg = 'Informe um link válido do youtube com a propriedade: /embed?';
+        return res.status(422).json({ error: true, msg});
+    }
+    
+    const result = await Class.update({ title, subtitle, resume, text, code, slides, video, score, xp } , { where: { id } });
+    
+    await transaction.commit();
+    
+    return res.status(200).json({ result });
+    
+} catch (error) {
+    await transaction.rollback();
+    console.log(error);
+    return res.status(422).json({ error: true, msg:error.message});
+}
+},
+
+
+destroy: async (req, res) => {
+    const transaction = await Class.sequelize.transaction();
+    try {
+        
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        
+        let { id } = req.params;
+        const foundClass = await Class.findByPk(id);
+        if (!foundClass) {
+            return res.status(422).json({ error: true, msg:'Não foi encontrada uma aula com o id especificado'});
+        }
+        const result = await foundClass.destroy();
+        
+        await transaction.commit();
+        
+        return res.status(200).json({ result });
+        
+    } catch (error) {
+        await transaction.rollback();
+        console.log(error);
+        return res.status(422).json({ error: true, msg: error.message});
+    }
+}
 
 
 };
--- routes/classes.js
+++ routes/classes.js
@@ -1,11 +1,48 @@
 const express = require('express');
 const router = express.Router();
 const classController = require('../controllers/classController');
-router.get('/all/:moduleId', classController.index);
-router.get('/:id', classController.show);
-router.post('/', classController.store);
-router.patch('/:id', classController.update);
-router.put('/:id', classController.update);
-router.delete('/:id', classController.destroy);
+const { check, param } = require('express-validator');
+
+router.get('/all/:moduleId', [
+    param('moduleId', 'Informe o id numérico de um módulo').isNumeric()
+], classController.index);
+
+router.get('/:id', [
+    param('id', 'Informe o id numérico da aula').isNumeric()
+], classController.show);
+
+router.post('/:moduleId', [
+    param('moduleId', 'Informe o id do módulo para vincular a aula').isNumeric(),
+    check('title','Informe um título para a aula').isLength({min:1, max:80}),
+    check('subtitle','Informe um subtítulo para a aula').isLength({min:1, max:80}),
+    check('slides','Informe um link para slides menor que 255 caracteres').isLength({max:255}),
+    check('video','Informe um link para video menor que 255 caracteres').isLength({min:1, max:255}),
+    check('score','Informe um score numérico para a aula').isNumeric(),
+    check('xp','Informe um xp numérico para a aula').isNumeric(),
+], classController.store);
+
+router.patch('/:id', [
+    param('id', 'Informe o id numérico da aula').isNumeric(),
+    check('title','Informe um título para a aula').isLength({min:1, max:80}),
+    check('subtitle','Informe um subtítulo para a aula').isLength({min:1, max:80}),
+    check('slides','Informe um link para slides menor que 255 caracteres').isLength({max:255}),
+    check('video','Informe um link para video menor que 255 caracteres').isLength({min:1, max:255}),
+    check('score','Informe um score numérico para a aula').isNumeric(),
+    check('xp','Informe um xp numérico para a aula').isNumeric(),
+], classController.update);
+
+router.put('/:id', [
+    param('id', 'Informe o id numérico da aula').isNumeric(),
+    check('title','Informe um título para a aula').isLength({min:1, max:80}),
+    check('subtitle','Informe um subtítulo para a aula').isLength({min:1, max:80}),
+    check('slides','Informe um link para slides menor que 255 caracteres').isLength({max:255}),
+    check('video','Informe um link para video menor que 255 caracteres').isLength({min:1, max:255}),
+    check('score','Informe um score numérico para a aula').isNumeric(),
+    check('xp','Informe um xp numérico para a aula').isNumeric(),
+], classController.update);
+
+router.delete('/:id', [
+    param('id', 'Informe o id numérico da aula').isNumeric()
+], classController.destroy);
 
 module.exports = router;
